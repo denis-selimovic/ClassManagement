d96e08d6a1a184554960832896795dcf97c68b09
dselimovic <dselimovic1@etf.unsa.ba>
2020-09-22 00:26:12 +0200

    Added POST route for changing setup

--- class-management-api/routes/assignments.js
+++ class-management-api/routes/assignments.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer();
 const Assignment = require('../model/Assignment');
+const Upload = require('../model/Upload');
 const { ROLE_TUTOR } = require('../common/roles');
 const { auth, checkRoles } = require('../common/auth');
 
@@ -10,6 +13,26 @@ router.patch('/:id', auth, checkRoles([ROLE_TUTOR]), async (req, res) => {
         res.status(200).json(assignment);
     } catch (e) {
         res.status(401).json({ message: 'Unable to access item' });
+    }
+});
+
+router.post('/:id/setup', auth, checkRoles([ROLE_TUTOR]), upload.single('file'), async (req, res) => {
+    try {
+        const assignment = await Assignment.findById(req.params.id).populate('setup');
+        if (!assignment || assignment.owner !== req.user._id.toString() || !req.file) {
+            return res.status(400).json({ message: 'Unable to access item' });
+        }
+        if (assignment.setup._id) {
+            await assignment.setup.remove();
+        }
+        const file = req.file;
+        const upload = new Upload({ mimetype: file.mimetype, data: file.buffer, name: file.originalname, owner: assignment._id.toString() });
+        await upload.save();
+        assignment.setup = upload;
+        await assignment.save();
+        res.status(201).json(assignment);
+    } catch (e) {
+        res.status(401).json({ message: 'Unable to update item' })
     }
 });
 
