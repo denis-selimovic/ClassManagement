9a8db400d781b2d8f76e75675c5f854ef182177c
dselimovic <dselimovic1@etf.unsa.ba>
2020-09-23 16:11:48 +0200

    Added auth components

--- class-management-webapp/src/app/app.module.ts
+++ class-management-webapp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const appRoutes: Routes = [
-  { path: '' }
+  { path: '', component: LoginComponent }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
