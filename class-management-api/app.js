eeeb90c73913e05a9d4688807dfe5756cd8d591b
dselimovic <dselimovic1@etf.unsa.ba>
2020-09-23 23:54:12 +0200

    Added CORS middleware to server

--- class-management-api/app.js
+++ class-management-api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 const app = express();
 
 const userRoutes = require('./routes/users');
@@ -8,6 +9,7 @@ const lessonRoutes = require('./routes/lessons');
 const assignmentRoutes = require('./routes/assignments');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/users', userRoutes);
