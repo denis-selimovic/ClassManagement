4cb74ebd05d122113602d24f9a3813a510cc78da
dselimovic <dselimovic1@etf.unsa.ba>
2020-09-24 01:09:48 +0200

    changed error messages

--- class-management-webapp/src/app/components/auth/register/register.component.ts
+++ class-management-webapp/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  failedRegistration = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  register(): void {
+    console.log(this.registerForm);
+  }
 }
