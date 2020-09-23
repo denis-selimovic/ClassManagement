cc5c994739827b13150717928b9306a05974c376
dselimovic <dselimovic1@etf.unsa.ba>
2020-09-23 23:15:34 +0200

    changed project structure

--- class-management-webapp/src/app/app.module.ts
+++ class-management-webapp/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NavbarComponent } from './auth/navbar/navbar.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { NavbarComponent } from './components/auth/navbar/navbar.component';
 
 const appRoutes: Routes = [
   { path: '', component: NavbarComponent, children: [
