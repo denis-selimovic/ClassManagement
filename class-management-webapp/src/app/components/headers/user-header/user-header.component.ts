a2033777804ed8d4ea7358399453c0c365ba1525
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-13 13:10:30 +0200

    implemented log out option

--- class-management-webapp/src/app/components/headers/user-header/user-header.component.ts
+++ class-management-webapp/src/app/components/headers/user-header/user-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from '../../../services/user/user.service';
 
 @Component({
   selector: 'app-user-header',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  toMainPage(): void {
+    this.router.navigate(['/dashboard']);
+  }
+
+  logOut(): void {
+    this.userService.clearUser();
+    this.router.navigate(['']);
+  }
 }
