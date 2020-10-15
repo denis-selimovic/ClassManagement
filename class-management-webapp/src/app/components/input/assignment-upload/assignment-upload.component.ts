e95271b92fd22db456f82ee9c110c33230ba107a
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-15 19:37:40 +0200

    finished controller for uploads

--- class-management-webapp/src/app/components/input/assignment-upload/assignment-upload.component.ts
+++ class-management-webapp/src/app/components/input/assignment-upload/assignment-upload.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { Assignment } from '../../../services/course/course.service';
 import { HttpClient } from '@angular/common/http';
 import { AssignmentService } from '../../../services/assignment/assignment.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-assignment-upload',
@@ -13,7 +14,13 @@ export class AssignmentUploadComponent implements OnInit {
   @Input() assignment: Assignment;
   @Output() hide = new EventEmitter<null>();
 
-  constructor(private http: HttpClient, private assignmentService: AssignmentService) { }
+  uploadForm: FormGroup;
+
+  constructor(private http: HttpClient, private assignmentService: AssignmentService, private formBuilder: FormBuilder) {
+    this.uploadForm = this.formBuilder.group({
+      file: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
@@ -23,6 +30,10 @@ export class AssignmentUploadComponent implements OnInit {
   }
 
   upload(): void {
-
+    const formData = new FormData();
+    formData.append('file', this.uploadForm.get('file').value);
+    this.assignmentService.upload(this.assignment._id, formData).subscribe(result => {
+      console.log(result);
+    });
   }
 }
