ac9dba7b22fc5fbb177e698c2617fd5107ea33fb
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-23 21:21:26 +0200

    changed navigation in menu

--- class-management-webapp/src/app/components/menus/course-main-menu/course-main-menu.component.ts
+++ class-management-webapp/src/app/components/menus/course-main-menu/course-main-menu.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Course} from '../../../services/course/course.service';
 import {UserService} from '../../../services/user/user.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-course-main-menu',
@@ -12,7 +13,7 @@ export class CourseMainMenuComponent implements OnInit {
   @Input() course: Course;
   tutor: string;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.userService.loadById(this.course.owner).subscribe(data => {
@@ -25,5 +26,9 @@ export class CourseMainMenuComponent implements OnInit {
       return 0;
     }
     return this.course.rating.total / this.course.rating.count;
+  }
+
+  navigate(link: string): any {
+    this.router.navigate([link], { relativeTo: this.route, state: { course: this.course } });
   }
 }
