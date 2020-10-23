56b8841c86db9ef6141fc3561bd258a0532a3ae6
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-23 22:32:12 +0200

    added lesson item component

--- class-management-webapp/src/app/components/lists/lesson-item/lesson-item.component.ts
+++ class-management-webapp/src/app/components/lists/lesson-item/lesson-item.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {NgbAccordion, NgbPanelChangeEvent} from '@ng-bootstrap/ng-bootstrap';
+import { saveAs } from 'file-saver';
+import {UserService} from '../../../services/user/user.service';
 
 @Component({
   selector: 'app-lesson-item',
@@ -8,10 +11,33 @@ import {Component, Input, OnInit} from '@angular/core';
 export class LessonItemComponent implements OnInit {
 
   @Input() lesson: any;
+  @Input() id: any;
+  @Input() owner: any;
+  uploads = {};
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  toggle($event: MouseEvent, a: NgbAccordion, panel: string): void {
+    $event.preventDefault();
+    (a.isExpanded(panel)) ? a.collapse(panel) : a.expand(panel);
+  }
+
+  downloadUpload(key: string): void {
+    const upload = this.uploads[key];
+    const blob = new Blob([upload.data], { type: upload.mimetype });
+    saveAs(blob, upload.name);
+  }
+
+  openUploadForm(): void {
+  }
+
+  load($event: NgbPanelChangeEvent): void {
+  }
+
+  checkOwnership(): boolean {
+    return this.owner === this.userService.getUser()._id;
+  }
 }
